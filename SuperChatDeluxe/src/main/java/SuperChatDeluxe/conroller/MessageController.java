7a9f3fbed95235bcdaffc86d849bc26ab9392b28
ATEOTG <bryansantos9797@gmail.com>
2024-02-12 17:40:46 -0500

    added adding message to database endpoint

--- SuperChatDeluxe/src/main/java/SuperChatDeluxe/conroller/MessageController.java
+++ SuperChatDeluxe/src/main/java/SuperChatDeluxe/conroller/MessageController.java
@@ -1,5 +1,6 @@
 package SuperChatDeluxe.conroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import SuperChatDeluxe.model.Message;
+import SuperChatDeluxe.service.MessageService;
 
 @RequestMapping("/api")
 @RestController
 public class MessageController {
 
-//	@PostMapping("/message")
-//	public ResponseEntity<?> sendMessage(@RequestBody Message newMessage){
-//		
-//	}
+	@Autowired
+	MessageService service;
+	
+	@PostMapping("/message")
+	public ResponseEntity<?> sendMessage(@RequestBody Message newMessage){
+		Message newAddedMessage = service.sendMessage(newMessage);
+		
+		return ResponseEntity.status(200).body(newAddedMessage);
+	}
+	
+	
 }
--- SuperChatDeluxe/src/main/java/SuperChatDeluxe/service/MessageService.java
+++ SuperChatDeluxe/src/main/java/SuperChatDeluxe/service/MessageService.java
@@ -1,5 +1,28 @@
 package SuperChatDeluxe.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import SuperChatDeluxe.model.Message;
+import SuperChatDeluxe.repository.MessageRepository;
+
+@Service
 public class MessageService {
 
+		@Autowired 
+		MessageRepository repo;
+		
+		public Message sendMessage(Message message) {
+			message.setMessageId(null);
+			Message newMessage = repo.save(message);
+			
+			return newMessage;
+		}
+		
+		public List<Message> getMessageHistory(){
+			
+			return null;
+		}
 }
