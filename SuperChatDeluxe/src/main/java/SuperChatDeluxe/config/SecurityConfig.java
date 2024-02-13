f38532de1f506c0a129060c070b6044d24a8fa35
ddb048 <106298312+ddb048@users.noreply.github.com>
2024-02-13 11:39:16 -0800

    updated security config

--- SuperChatDeluxe/src/main/java/SuperChatDeluxe/config/SecurityConfig.java
+++ SuperChatDeluxe/src/main/java/SuperChatDeluxe/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package SuperChatDeluxe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,13 +16,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests(authz -> authz
-                        .antMatchers("/register").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic();
-        return http.build();
+        http.csrf().disable()
+        .authorizeHttpRequests()
+        .requestMatchers(AUTH_WHITE_LIST).permitAll()
+        .requestMatchers(HttpMethod.POST,"/api/admin/").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.PATCH,"/api/admin/").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.POST,"/api/register/").permitAll()
+        .requestMatchers(HttpMethod.PATCH,"/api/registration/").permitAll()
+        .requestMatchers(HttpMethod.GET,"/api/users/search/").permitAll()
+        .requestMatchers(HttpMethod.GET,"/api/recipes/favorites/users/").permitAll()
+        .requestMatchers(HttpMethod.GET,"/api/recipes/search/").permitAll()
+        .requestMatchers(HttpMethod.GET,"/api/users").permitAll()
+        .requestMatchers(HttpMethod.GET,"/api/users/").permitAll()
+        .requestMatchers("/authenticate").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
